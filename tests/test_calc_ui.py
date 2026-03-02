e84368db8e1e4db1205c787bd64642c14a9ffe62
rathishmr <43414601+rathishmr@users.noreply.github.com>
2026-03-02 12:45:46 +0530

    Update test_calc_ui.py

--- tests/test_calc_ui.py
+++ tests/test_calc_ui.py
@@ -5,18 +5,16 @@ from selenium.webdriver.common.by import By
 import os
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")  
 def driver():
     chrome_options = Options()
 
-    # CI Safe Options
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
 
-    # ✅ Selenium Manager handles driver automatically
     driver = webdriver.Chrome(options=chrome_options)
 
     file_path = os.path.abspath("calculator.html")
